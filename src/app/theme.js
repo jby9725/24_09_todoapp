2458ec4535a806315a9e041c38ac591c354c01ff
jby9725 <jby9725@gmail.com>
2024-09-30 15:53:10 +0900

    Todo 추가 폼 꾸미기, TextField, Button

--- src/app/theme.js
+++ src/app/theme.js
@@ -1,6 +1,9 @@
 import { createTheme } from '@mui/material';
 import { create } from '@mui/material/styles/createTransitions';
 const theme = createTheme({
+  typography: {
+    fontFamily: 'GmarketSansMedium',
+  },
   palette: {
     type: 'light',
     primary: {
