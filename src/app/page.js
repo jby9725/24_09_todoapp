842a5438bc9b6cb7f92742ddc2f6689a849a3597
jby9725 <jby9725@gmail.com>
2024-09-30 14:02:12 +0900

    Mui Backdrop

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,15 @@
 'use client';
 import Image from 'next/image';
 import * as React from 'react';
-import { AppBar, Toolbar, Button, Alert as MuiAlert, Snackbar } from '@mui/material';
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Alert as MuiAlert,
+  Snackbar,
+  Backdrop,
+  CircularProgress,
+} from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
 import { FaBars } from 'react-icons/fa';
 import theme from './theme';
@@ -12,8 +20,6 @@ const Alert = React.forwardRef((props, ref) => {
 
 export default function App() {
   const [open, setOpen] = React.useState(false);
-
-  const alertRef = React.useRef(null);
 
   return (
     <>
@@ -37,25 +43,15 @@ export default function App() {
         </section>
       </ThemeProvider>
 
-      <section>
-        <Button onClick={() => setOpen(true)}>Open Snackbar</Button>
-        <Alert ref={alertRef} severity="error" variant="filled">
-          게시물이 삭제됨
-        </Alert>
-        <Alert ref={alertRef} severity="success" variant="outlined">
-          성공 메세지
-        </Alert>
-        <Snackbar
+      <div>
+        <Button onClick={() => setOpen(true)}>Show backdrop</Button>
+        <Backdrop
+          sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
           open={open}
-          autoHideDuration={2000}
-          onClose={() => setOpen(false)}
-          message="Note archived">
-          <Alert variant="outlined" severity="error">
-            게시물 삭제{' '}
-            {/* 해당 Alert의 유무에 따라서 스낵바를 클릭했을 때 나오는 메세지가 달라짐*/}
-          </Alert>
-        </Snackbar>
-      </section>
+          onClick={() => setOpen(false)}>
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </div>
     </>
   );
 }
