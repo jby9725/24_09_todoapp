c6b16ff0609c9ffc43883cf9e18abbc8fab63aea
jby9725 <jby9725@gmail.com>
2024-09-30 11:30:26 +0900

    MUI 테마 커스터마이징, React Icons

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,43 @@
 'use client';
 import Image from 'next/image';
+import * as React from 'react';
 import Button from '@mui/material/Button';
+import { ThemeProvider } from '@emotion/react';
+import { RiDeleteBin5Line } from 'react-icons/ri';
+import theme from './theme';
 
 export default function Home() {
   return (
     <>
-      <Button className="tw-bg-red-500" variant="contained">
-        버튼
-      </Button>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
-          안녕
+      <ThemeProvider theme={theme}>
+        <div className="tw-flex tw-items-center tw-gap-x-3">
+          <Button variant="text">Text</Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              confirm('삭제 할거야?');
+            }}>
+            <RiDeleteBin5Line />
+            삭제
+          </Button>
+          <Button variant="outlined">Outlined</Button>
         </div>
-      </main>
+      </ThemeProvider>
+      <div className="tw-flex tw-items-center tw-gap-x-3 tw-mt-3">
+        <Button
+          variant="text"
+          onClick={() => {
+            alert('버튼 클릭 됨');
+          }}>
+          Text
+        </Button>
+        <Button variant="contained" disabled>
+          Contained
+        </Button>
+        <Button variant="outlined" href="sub/">
+          sub 로 이동
+        </Button>
+      </div>
     </>
   );
 }
---
+++ src/app/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+import { create } from '@mui/material/styles/createTransitions';
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#f50057',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+});
+export default theme;
