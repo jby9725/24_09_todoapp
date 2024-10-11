7fb4dedea6909ba246748a9e60f98931775f14ad
jby9725 <jby9725@gmail.com>
2024-10-11 16:37:24 +0900

    mui theme color 변수화, content 3등분

--- src/app/page.js
+++ src/app/page.js
@@ -13,8 +13,9 @@ import {
 } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
 import { FaBars } from 'react-icons/fa';
-import theme from './theme';
+// import theme from './theme';
 import dateToStr from './dateUtil';
+import RootTheme from './theme';
 
 function useTodoStatus() {
   console.log('실행 1');
@@ -220,8 +221,16 @@ const App = () => {
                   className="tw-pt-3"
                   label={`날짜 : ${todo.regDate}`}
                   variant="outlined"></Chip>
-                <div className="tw-p-8 tw-rounded-[15px] tw-shadow tw-whitespace-pre-wrap tw-leading-relaxed tw-break-words">
-                  <Box sx={{ color: 'secondary.dark' }}>할 일 : {todo.content}</Box>
+                <div className="tw-flex tw-p-8 tw-rounded-[15px] ">
+                  <Button className="tw-flex-shrink-0 tw-rounded-[20px_0_0_20px] hover:tw-bg-blue-300 tw-items-center">
+                    <span>체크박스</span>
+                  </Button>
+                  <div className="tw-flex-grow tw-text-[--mui-color-success-main] hover:tw-text-[--mui-color-info-main] tw-shadow tw-whitespace-pre-wrap tw-leading-relaxed tw-break-words">
+                    할 일 : {todo.content}
+                  </div>
+                  <div className="tw-flex-shrink-0 tw-bg-green-500 tw-w-[150px]">
+                    삭제 버튼 예정
+                  </div>
                 </div>
               </div>
             </li>
@@ -233,6 +242,7 @@ const App = () => {
 };
 
 export default function themeApp() {
+  const theme = RootTheme();
   console.log('실행 2');
 
   return (
--- src/app/theme.js
+++ src/app/theme.js
@@ -1,18 +1,51 @@
 import { createTheme } from '@mui/material';
-import { create } from '@mui/material/styles/createTransitions';
-const theme = createTheme({
-  typography: {
-    fontFamily: 'GmarketSansMedium',
-  },
-  palette: {
-    type: 'light',
-    primary: {
-      main: '#f50057',
-      dark: '#afafaf',
+import * as React from 'react';
+
+const muiThemePaletteKeys = [
+  'background',
+  'error',
+  'info',
+  'primary',
+  'secondary',
+  'success',
+  'text',
+  'warning',
+];
+
+export default function RootTheme() {
+  const theme = createTheme({
+    typography: {
+      fontFamily: 'GmarketSansMedium',
     },
-    secondary: {
-      main: '#f50057',
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#3f51b5',
+      },
+      secondary: {
+        main: '#f50057',
+      },
+      background: {
+        default: '#fff',
+        paper: '#fff',
+      },
     },
-  },
-});
-export default theme;
+  });
+
+  React.useEffect(() => {
+    const r = document.querySelector(':root');
+    muiThemePaletteKeys.forEach((paletteKey) => {
+      const themeColorObj = theme.palette[paletteKey];
+      // console.log(themeColor);
+      for (const key in themeColorObj) {
+        // console.log(key);
+        if (Object.hasOwnProperty.call(themeColorObj, key)) {
+          const colorVal = themeColorObj[key];
+          r.style.setProperty(`--mui-color-${paletteKey}-${key}`, colorVal);
+        }
+      }
+    });
+  }, []);
+
+  return theme;
+}
